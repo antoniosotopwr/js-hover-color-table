656c6b49a8e478843b79e8997190b99315c7b3f5
Antonio Soto <antoniosotopwr@gmail.com>
2022-12-08 12:53:06 -0600

    change color for all the squares added

--- script.js
+++ script.js
@@ -2,35 +2,69 @@
 
 const div1 = document.querySelector(".div-1");
 
-div1.addEventListener("mouseover", (e) => {
-  let hexNumbers = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "A",
-    "B",
-    "C",
-    "D",
-    "F",
-  ];
-  let randomColor = "";
-  let randomNumber;
+const card = document.querySelector(".card");
 
-  for (let i = 0; i < 6; i++) {
-    randomNumber = Math.floor(Math.random() * 15);
-    randomColor += hexNumbers[randomNumber];
+card.addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("square")) {
+    let hexNumbers = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "A",
+      "B",
+      "C",
+      "D",
+      "F",
+    ];
+    let randomColor = "";
+    let randomNumber;
+
+    for (let i = 0; i < 6; i++) {
+      randomNumber = Math.floor(Math.random() * 15);
+      randomColor += hexNumbers[randomNumber];
+    }
+    console.log(randomColor);
+
+    e.target.style.backgroundColor = "#" + randomColor;
   }
-  console.log(randomColor);
-
-  e.target.style.backgroundColor = "#" + randomColor;
 });
+
+// div1.addEventListener("mouseover", (e) => {
+//   let hexNumbers = [
+//     "0",
+//     "1",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//     "A",
+//     "B",
+//     "C",
+//     "D",
+//     "F",
+//   ];
+//   let randomColor = "";
+//   let randomNumber;
+
+//   for (let i = 0; i < 6; i++) {
+//     randomNumber = Math.floor(Math.random() * 15);
+//     randomColor += hexNumbers[randomNumber];
+//   }
+//   console.log(randomColor);
+
+//   e.target.style.backgroundColor = "#" + randomColor;
+// });
 
 // div1.addEventListener("mouseleave", (e) => {
 //   e.target.style.backgroundColor = "gray";
